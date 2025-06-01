690df6bfae6678b4231093c896e0fe2b81b96b91
geunyong0610 <petty001@naver.com>
2025-06-02 02:45:12 +0900

    코드 합침

--- screenGraderesult.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef SCREENGRADERESULT_HPP
-#define SCREENGRADERESULT_HPP
-
-#include <SFML/Graphics.hpp>
-#include <string>
-
-int screenGradeResult(sf::RenderWindow& window, int grade, int score);
-
-#endif
---
+++ src/screenEnd.hpp
@@ -0,0 +1,8 @@
+#ifndef SCREENEND_HPP
+#define SCREENEND_HPP
+
+#include <SFML/Graphics.hpp>
+
+int screenEnd(sf::RenderWindow& window);
+
+#endif
